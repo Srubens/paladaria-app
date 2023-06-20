213d26b8d57fc0599cfdd81d3358ad9b4826848c
Rubens Filipe <rubensfilipe6@gmail.com>
2023-06-20 14:48:57 -0300

    modificando arquivos assets, criando components, criando o services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderTopoComponent } from './components/header-topo/header-topo.component';
+import { HomeComponent } from './components/home/home.component';
+import { OrdemCompraComponent } from './components/ordem-compra/ordem-compra.component';
+import { ProdutoComponent } from './components/produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderTopoComponent,
+    HomeComponent,
+    OrdemCompraComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header-topo/header-topo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-topo',
+  templateUrl: './header-topo.component.html',
+  styleUrls: ['./header-topo.component.css']
+})
+export class HeaderTopoComponent {
+
+}
